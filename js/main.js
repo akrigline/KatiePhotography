59963a6ee3ecf0e5d6d261b307f533a59917e0c3
akrigline <andrew@akrigline.com>
2014-12-19 10:46:55 -0500

    Something to initialize the plugins

--- js/main.js
+++ js/main.js
@@ -7,32 +7,32 @@
 	$body = $('body');
 	
     //FadeIn all sections   
-	$body.imagesLoaded( function() {
-		setTimeout(function() {
-		      
+    $body.imagesLoaded( function() {
+    	setTimeout(function() {
+    		
 		      // Resize sections
 		      adjustWindow();
 		      
 		      // Fade in sections
-			  $body.removeClass('loading').addClass('loaded');
-			  
-		}, 800);
-	});
-	
-	function adjustWindow(){
-		
+		      $body.removeClass('loading').addClass('loaded');
+		      
+		  }, 800);
+    });
+    
+    function adjustWindow(){
+    	
 		// Init Skrollr
 		var s = skrollr.init({
-    forceHeight: false
-});
+			forceHeight: false
+		});
 		
 		// Get window size
-	    winH = $window.height();
-	    
+		winH = $window.height();
+		
 	    // Keep minimum height 550
 	    if(winH <= 550) {
-			winH = 550;
-		} 
+	    	winH = 550;
+	    } 
 	    
 	    // Resize our slides
 	    $slide.height(winH);
@@ -41,5 +41,5 @@
 	    s.refresh($('.homeSlide'));
 	    
 	}
-		
+	
 } )( jQuery );
