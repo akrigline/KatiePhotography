cb02d9945d07fab6a20f0c6681d1b1a93ec27495
akrigline <andrew@akrigline.com>
2015-01-10 17:59:51 -0500

    remove jssor, implement bxslider

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@
 		      // Fade in sections
 		      $body.removeClass('loading').addClass('loaded');
 		      
-		  }, 800);
+		  }, 100);
     });
     
     function adjustWindow(){
@@ -43,46 +43,3 @@
 	}
 
 } )( jQuery );
-
-
-jQuery(document).ready(function ($) {
-
-    var options = {
-        $AutoPlay: false,                                    //[Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
-        
-        $ArrowKeyNavigation: true,   			            //[Optional] Allows keyboard (arrow key) navigation or not, default value is false
-        $SlideDuration: 800,                                //Specifies default duration (swipe) for slide in milliseconds
-
-        $ArrowNavigatorOptions: {                       //[Optional] Options to specify and enable arrow navigator or not
-            $Class: $JssorArrowNavigator$,              //[Requried] Class to create arrow navigator instance
-            $ChanceToShow: 2                               //[Required] 0 Never, 1 Mouse Over, 2 Always
-        },
-
-        $ThumbnailNavigatorOptions: {                       //[Optional] Options to specify and enable thumbnail navigator or not
-            $Class: $JssorThumbnailNavigator$,              //[Required] Class to create thumbnail navigator instance
-            $ChanceToShow: 2,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
-
-            $ActionMode: 1,                                 //[Optional] 0 None, 1 act by click, 2 act by mouse hover, 3 both, default value is 1
-            $SpacingX: 8,                                   //[Optional] Horizontal space between each thumbnail in pixel, default value is 0
-            $DisplayPieces: 10,                             //[Optional] Number of pieces to display, default value is 1
-            $ParkingPosition: 360                          //[Optional] The offset position to park thumbnail
-        }
-    };
-
-    var jssor_slider1 = new $JssorSlider$("gallery", options);
-    //responsive code begin
-    //you can remove responsive code if you don't want the slider scales while window resizes
-    function ScaleSlider() {
-        var parentWidth = jssor_slider1.$Elmt.parentNode.clientWidth;
-        if (parentWidth)
-            jssor_slider1.$ScaleWidth(Math.max(Math.min(parentWidth, 800), 300));
-        else
-            window.setTimeout(ScaleSlider, 30);
-    }
-    ScaleSlider();
-
-    $(window).bind("load", ScaleSlider);
-    $(window).bind("resize", ScaleSlider);
-    $(window).bind("orientationchange", ScaleSlider);
-    //responsive code end
-});
